48ce96ef25cb5d73ead3b4e98552228b1d8f5c1a
pixelpanda764 <mandiequartly@gmail.com>
2024-09-08 11:09:00 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Things about goats :goat:")
+st.write("Let's get started! Answer this question to start...")
+
+col1, col2, col3, col4 = st.columns([2, 1, 1, 5])
+
+with col1:
+    st.write("Are goats amazing?")
+
+with col2:
+    button1 = st.button('Yes')
+    
+with col3:
+    button2 = st.button('No')
+    
+if button1:
+    q_agreement = True
+    st.write("You clicked yes!")
+
+if button2:
+    q_agreement = False
+    st.write("Sorry you need to agree to continue")
